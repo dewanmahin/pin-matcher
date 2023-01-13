a18001047ad561c0f5a2ee0df26266d982a9eeed
DEWAN <dewanmahinchy@gmail.com>
2023-01-13 22:08:20 +0600

    calculator number collecting using event delegate and verify pin code added

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ function getPin(){
     if(pinString.length == 4){
         return pin;
     }else{
-        // console.log('4 digit pin has not been found', pin);
         return getPin();
     }
 }
@@ -18,4 +17,40 @@ document.getElementById('generate-pin').addEventListener('click', function(){
     const pin = getPin();
     const displayPinField = document.getElementById('display-pin');
     displayPinField.value = pin;
+})
+
+document.getElementById('calculator').addEventListener('click', function(event){
+    const number = event.target.innerText;
+    const typedNUmberField = document.getElementById('typed-numbers');
+    let previousTypedNumber = typedNUmberField.value;
+    if(isNaN(number)){
+        if(number === 'C'){
+            typedNUmberField.value = '';
+        }else if(number === '<'){
+            const digits = previousTypedNumber.split('');
+            digits.pop();
+            const remainingDigits = digits.join('');
+            typedNUmberField.value = remainingDigits;
+        }
+    }else{
+        const newTypedNumber = previousTypedNumber + number;
+        typedNUmberField.value = newTypedNumber;
+    }
+})
+
+document.getElementById('verify-pin').addEventListener('click', function(){
+    const displayPinField = document.getElementById('display-pin');
+    const currentPin = displayPinField.value;
+    const typedNUmberField = document.getElementById('typed-numbers');
+    const typedNUmber = typedNUmberField.value;
+    // alert message element
+    const pinCorrectMsg = document.getElementById('pin-correct');
+    const pinIncorrectMsg = document.getElementById('pin-incorrect');
+    if(currentPin === typedNUmber){
+        pinIncorrectMsg.style.display = 'none'; //Incorrect message hide
+        pinCorrectMsg.style.display = 'block'; //Correct message Show
+    }else{
+        pinCorrectMsg.style.display = 'none'; //Correct message hide
+        pinIncorrectMsg.style.display = 'block'; //Incorrect message Show
+    }
 })
